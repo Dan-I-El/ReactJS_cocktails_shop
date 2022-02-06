9850f1003306382ede3c62c1aed0b9f30b28ad10
Dan-I-El <maysonb901@gmail.com>
2022-02-06 18:28:36 +0200

    Uploaded to Github

---
+++ src/pages/Error.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>
+          Oops! It's a dead end.
+        </h1>
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
+      </div>
+      <h2>error pag </h2>
+    </section>
+  )
+}
+
+export default Error
